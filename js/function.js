76116f2d4775ac3790c4f27e08b01ba36ac13d3e
adibahbab4108 <adibalvi476@gmail.com>
2024-09-24 22:48:47 +0600

    date function is updated

--- js/function.js
+++ js/function.js
@@ -28,26 +28,16 @@ function getDonationdLocationName(donateBtn) {
 }
 
 
-//Time, Date and Location
-let dateTimeLocation = '';
-const date = new Date();
-const options = {
-    weekday: 'short', year: 'numeric', month: 'short', day: 'numeric',
-    hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short'
-};
-
-const formattedDate = date.toLocaleString('en-US', options);
-const countryName = new Intl.DisplayNames([navigator.language], { type: 'region' }).of('BD');
-dateTimeLocation = `${formattedDate} (${countryName} Standard Time)`;
 
 
 function addTransactionToHistory(donatedAmount) {
+    const date = new Date();
     if (donatedAmount <= 0) return;
     else {
         document.getElementById('history-section').innerHTML += `
          <div class="p-6 border rounded-lg">
                 <h2 class="font-bold mb-4">${donatedAmount} Taka is Donated for ${text} </h2>
-                <small class="text-gray-400"> Date: ${dateTimeLocation} </small>
+                <small class="text-gray-400"> Date: ${date} </small>
             </div>
     `
     }
